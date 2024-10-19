4a737bd553c6f8ff5cb1b6b4ee87c485f9923487
Farid Bass <abdoulfaridbassirou7898@gmail.com>
2024-10-19 21:56:08 +0300

    user authentication and authorization

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,15 +3,44 @@ let books = require('../config/booksdb')
 let users = require('../config/usersdb').users
 
 const isValid = (username) => {
-    
+    const validUsers = users.filter((user)=>{
+        return user.username === username;	
+    })
+    return validUsers.length > 0
 }
 
 const authenticatedUser = (username,password) => {
-
+    const authenticatedUsers = users.filter((user)=>{
+    	return user.username === username && user.password === password;
+    })
+    return authenticatedUsers.length > 0
 }
 
 const login = (req,res) => {
+	const {username,password} = req.body;
+    
+    if (!isValid(username)) {
+    	return res.status(401).json({
+    		message: "User is not valid"
+    	})
+    }
 
+    if (authenticatedUser(username,password)) {
+    	const token = jwt.sign({username},'faridSecretKey',{expiresIn:60*2})
+
+    	req.session.authorization = {
+    		token,username
+    	}
+
+    	return res.status(200).json({
+    		message: "User logged successfully",
+    		token
+    	})
+    }
+
+    return res.status(208).json({
+    	message: "Invalid login. Check username or password"
+    })
 }
 
 const addBookReview = (req,res) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,30 @@
 const jwt = require('jsonwebtoken')
 
 const auth = (req,res,next) => {
+	if (req.session.authorization) {
+		const token = req.session.authorization['token']
 
+		if(!token) {
+			return res.status(401).json({
+				message: "Access denied,no token provided"
+			})
+		}
+
+		try {
+			const decoded = jwt.verify(token,'faridSecretKey')
+            req.username = decode.username
+            next()
+		} catch (error) {
+			return res.status(403).json({
+				message: 'User not logged in'
+			})
+		}
+
+	} else {
+	return res.status(403).json({
+    message: "User not logged in"
+	})
+}
 }
 
 module.exports = auth;
